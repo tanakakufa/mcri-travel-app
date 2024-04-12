b2cd80c97ea00bd128a86377aea172d809392250
Patrick Nelson <prnelson@mainsl.com>
2024-04-12 09:34:36 -0500

    feature: add api call to PackingList page to get items to pack

--- src/pages/PackingList/PackingList.tsx
+++ src/pages/PackingList/PackingList.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import Header from "../../reusables/Header/Header";
 
 const PackingList = () => {
+  const [packingList, setPackingList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/itemsToPack', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        return setPackingList(data)
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  console.log("packingList:", packingList)
   
   return (
     <>
