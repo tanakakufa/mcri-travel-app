ff147e5e57c0695035c68c914f9d15941be329de
Patrick Nelson <prnelson@mainsl.com>
2024-04-12 13:06:08 -0500

    feature: get data for flights and start displaying on page

--- src/pages/Flights/Flights.tsx
+++ src/pages/Flights/Flights.tsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
 import Header from "../../reusables/Header/Header";
 import PageTitle from "../../reusables/PageTitle/PageTitle";
 
 const Flights = () => {
+  const [flights, setFlights] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/flights', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        return setFlights(data)
+      })
+      .catch(error => console.error(error));
+  }, []);
   
   return (
     <>
@@ -12,6 +30,31 @@ const Flights = () => {
           title="Flights"
           emoji="✈️"
         />
+
+        <div>
+          {flights.map((flight: any) => {
+            return (
+              <div>
+                <div>
+                  <p>Departure</p>
+                  <p>{flight.departureDateTime}</p>
+                  <p>{flight.departureCity}</p>
+                  <p>{flight.departureAirport}</p>
+                </div>
+                <div>
+                  <p>flight time</p>
+                  <p>{flight.airline}</p>
+                </div>
+                <div>
+                  <p>Arrival</p>
+                  <p>{flight.arrivalDateTime}</p>
+                  <p>{flight.arrivalCity}</p>
+                  <p>{flight.arrivalAirport}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
