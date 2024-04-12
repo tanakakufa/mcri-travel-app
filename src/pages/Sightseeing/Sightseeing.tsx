47aeedc2ab45b326efc241fb18e2af86c9c5892e
Patrick Nelson <prnelson@mainsl.com>
2024-04-12 14:12:55 -0500

    feature: get sightseeing data and start displaying on page

--- src/pages/Sightseeing/Sightseeing.tsx
+++ src/pages/Sightseeing/Sightseeing.tsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
 import Header from "../../reusables/Header/Header";
 import PageTitle from "../../reusables/PageTitle/PageTitle";
 
 const Sightseeing = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/thingsToSee', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        return setActivities(data)
+      })
+      .catch(error => console.error(error));
+  }, []);
   
   return (
     <>
@@ -12,6 +30,25 @@ const Sightseeing = () => {
           title="Sightseeing"
           emoji="🗺️"
         />
+
+        <div>
+          {activities.map((activity: any) => {
+            return (
+              <div>
+                <div>
+                  <img src={activity.image} />
+                </div>
+                <p>{activity.name}</p>
+                <p>{activity.address}</p>
+                <p><b>Intensity level:</b><span>{activity.intensity}</span></p>
+
+                <div>
+                  <p>{activity.type}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
