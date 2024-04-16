5b9b74e53e61a27edd096bf7384b329dcdfd7048
Patrick Nelson <prnelson@mainsl.com>
2024-04-16 10:22:30 -0500

    feature: resources page

--- src/pages/Resources/Resources.tsx
+++ src/pages/Resources/Resources.tsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
 import Header from "../../reusables/Header/Header";
 import PageTitle from "../../reusables/PageTitle/PageTitle";
+import "./Resources.css";
 
 const Resources = () => {
+  const [resourceLinks, setResourceLinks] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/resources', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        return setResourceLinks(data)
+      })
+      .catch(error => console.error(error));
+  }, []);
+
   
   return (
     <>
@@ -12,6 +32,17 @@ const Resources = () => {
           title="Resources"
           emoji="🖥️"
         />
+
+        <div className="all-links-container">
+          {resourceLinks.map((link: any) => {
+            return (
+              <div className="link-card" key={link.id}>
+                <a href={link.id === "4003" ? "" : link.url} target="_blank">{link.name}</a>
+                <p>🔗</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
